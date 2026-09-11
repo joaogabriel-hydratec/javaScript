b9e7f3c34326bb6caf5400f7eb5beb025dea580b
joao gabriel <jgcpinhedo@gmail.com>
2026-09-11 09:10:16 -0300

    add

---
+++ tipos-de-dados-cadeia.js
@@ -0,0 +1,7 @@
+let nome = "Luiz"; //string com aspas duplas
+let sobrenome = "carlos"; // string com aspas simples
+let saudacao = `olá, ${nome}!`; // template literal com interpolação de variáveis
+
+console.log ("meu nome é", nome)
+console.log ("meu sobrenome é", sobrenome)
+console.log (saudacao)
---
+++ tipos-de-dados-number.js
@@ -0,0 +1,7 @@
+let idade = 25; // numero inteiro
+let altura = 1.75; // numero com ponto flutuante
+let temperatura = -5 // número negativo
+
+console.log ( "idade",idade)
+console.log ( "altura",altura)
+console.log ( "temperatura",temperatura)
