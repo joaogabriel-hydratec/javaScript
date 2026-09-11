0eda32abec0270b4a342eaf68f019f2b5d1ea5c4
joao gabriel <jgcpinhedo@gmail.com>
2026-09-11 10:43:15 -0300

    add

---
+++ tipos-de-dados-naoprimitivo-array.js
@@ -0,0 +1,7 @@
+let frutas = ["maçã", "banana", "laranja"]; //array de strings 
+let numeros = [1,2,3,4,5]; // array de números
+let misto = [true, "texto", 42]; // array com diferentes tipos de dados]
+
+console.log (frutas)
+console.log (numeros)
+console.log (misto)
