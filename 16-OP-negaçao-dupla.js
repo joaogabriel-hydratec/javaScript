0b7e70afe28cbad776687cedd35961107571f2b5
joao gabriel <jgcpinhedo@gmail.com>
2026-09-18 11:19:51 -0300

    add

---
+++ 16-OP-negaçao-dupla.js
@@ -0,0 +1,10 @@
+// valores falsy
+console.log(!!0);
+console.log(!!"");
+console.log(!!null);
+
+//valores truthy
+console.log(!!1);
+console.log(!!"texto");
+console.log(!![]);
+console.log(!!{});
