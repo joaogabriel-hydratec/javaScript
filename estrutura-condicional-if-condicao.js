3b6f3bc9c15f8198886424ff66cbc356bcdab6d2
joao gabriel <jgcpinhedo@gmail.com>
2026-09-22 08:40:45 -0300

    add

--- estrutura-condicional-if-condicao.js
+++ estrutura-condicional-if-condicao.js
@@ -6,4 +6,4 @@ if (hora < 12) {
     console.log("bom dia!!!")
     }
     // o rstaste do codigo continua independente se a condição for verdadeira ou não.
-    console.log("dinheiro não traz felicidade. me dê o seu e seja feliz 🤣🤣🤣")
+    console.log("dinheiro não traz felicidade. me dê o seu e seja feliz 🤣🤣🤣");
